2a9a8f3a4ebeb898719f9576eea1b7fa03bbef0e
michael2995 <msg@zigbang.com>
2021-03-30 09:36:31 +0900

    chore: refactor

--- src/task/update.ts
+++ src/task/update.ts
@@ -4,32 +4,34 @@ import queryString from "querystring"
 
 const {SERVER_ENDPOINT} = process.env
 
+const updateExisting = (existing: Person & {id: number}, updated: Person) => {
+    return fetch(SERVER_ENDPOINT, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: existing.id,
+            ...updated,
+        })
+    })
+}
+
+const writePerson = (person: Person) => {
+    return fetch(SERVER_ENDPOINT, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(person)
+    })
+}
+
 export const update = async (person: Person) => {
     if (!SERVER_ENDPOINT) throw Error("Couldn't find endpoint")
 
     try {
-        const res = await fetch(`${SERVER_ENDPOINT}?${queryString.stringify(person)}`)
+        const query = queryString.stringify(person)
+        const res = await fetch(`${SERVER_ENDPOINT}?${query}`)
         const existing = res.status === 200 && await res.json()
-        if (existing) {
-            await fetch(SERVER_ENDPOINT, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    id: existing.id,
-                    ...person,
-                })
-            })
-        } else {
-            await fetch(SERVER_ENDPOINT, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(person)
-            })
-        }
+        if (existing) return await updateExisting(existing, person)
+        return await writePerson(person)
     } catch (e) {
         console.log(e)
     }
